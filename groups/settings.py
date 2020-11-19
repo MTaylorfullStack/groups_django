bea9f012f4bbbf85f94463dc098f4924d8c8cc2d
MTaylorfullStack <mtaylor@codingdojo.com>
2020-11-18 19:21:23 -0600

    new files

--- groups/settings.py
+++ groups/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secrets import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$8c-i#xt_=t=gj+_u(5xw_p!2+v5r$+*=ha3ax&#&a#bvy@#=8'
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
